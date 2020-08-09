ca51bc83f1ef91f9b3d44a3a594bc911e9545ebf
akhisulaymaan <54278218+akhisulaymaan@users.noreply.github.com>
2020-08-09 14:05:16 +0100

    Add files via upload

---
+++ for_loop_range.py
@@ -0,0 +1,6 @@
+# Write a for loop using range() to print out multiples of 5 up to 30 inclusive
+
+multiples = 5
+for five in range(1, 31):
+
+    print(multiples * five)
---
+++ zip_dict.py
@@ -0,0 +1,11 @@
+#Zip Lists to a Dictionary
+#Use zip to create a dictionary cast that uses names as keys and heights as values.
+
+cast_names = ["Barney", "Robin", "Ted", "Lily", "Marshall"]
+cast_heights = [72, 68, 72, 66, 76]
+
+for i, label in enumerate(labels):
+    points.append(label + ": " + str(x_coord[i]) + ', ' + str(y_coord[i]) + ', ' + str(z_coord[i]))
+
+for point in points:
+print(cast)
